8d0ad40114dfb509056bb488512bf9246893daed
RafaelGuido <rafatocco7@gmail.com>
2021-09-23 18:01:24 -0300

    subindo codigo pro repositório

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { timingSafeEqual } from 'crypto';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor!: number;
+  destino!: number;
+
+  transferir() {
+    console.log('nova transferência solicitada');
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    // console.log('Valor:', this.valor);
+    // console.log('Destino:', this.destino);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
